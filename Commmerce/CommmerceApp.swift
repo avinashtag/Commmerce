181d8d46df915a28858540441c3a4f444ab2a80c
Avinash Tag <avi.tag@outlook.com>
2024-11-19 14:14:27 +0800

    First View Tabbar

--- Commmerce/CommmerceApp.swift
+++ Commmerce/CommmerceApp.swift
@@ -25,7 +25,7 @@ struct CommmerceApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabBarView()
         }
         .modelContainer(sharedModelContainer)
     }
